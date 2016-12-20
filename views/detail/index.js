9d6bacdf2c0c787fe0e98ad5f7abe9191e149299
lizongqing <lizongqing.sh@superjia.com>
2016-12-20 15:38:22 +0800

    demo

---
+++ views/detail/index.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+
+export default class PageDetail extends Component{
+    render(){
+        return(
+            <div>detail</div>
+        );
+    }
+}
---
+++ views/home/index.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+
+export default class PageHome extends Component{
+    render(){
+        return(
+            <div>Home</div>
+        );
+    }
+}
